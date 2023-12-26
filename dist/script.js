f81870c27f0193522e7d6fa16ad7d9d2894bb256
HerlynMae <torresherlynmae@gmail.com>
2023-12-26 16:32:01 +0800

    first commit

---
+++ dist/script.js
@@ -0,0 +1,10 @@
+// import { tns } from "./src/tiny-slider.js";
+
+var slider = tns({
+  container: ".my-slider",
+  items: 1,
+  slideBy: "page",
+  autoplay: true,
+  prevButton: false,
+  nextButton: false,
+});
