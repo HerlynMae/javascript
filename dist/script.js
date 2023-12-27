da334af4ae7c5793c6307bfb4fcd5e8d2ef98d5c
HerlynMae <torresherlynmae@gmail.com>
2023-12-27 15:20:56 +0800

    update

--- dist/script.js
+++ dist/script.js
@@ -1,10 +1,47 @@
-// import { tns } from "./src/tiny-slider.js";
-
+// slider
 var slider = tns({
   container: ".my-slider",
   items: 1,
+  gutter: 100,
   slideBy: "page",
   autoplay: true,
-  prevButton: false,
-  nextButton: false,
+  controls: false,
+  navPosition: "bottom",
 });
+
+// accordion
+const accordions = document.querySelectorAll("h4");
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    accordion.nextElementSibling.classList.toggle("open");
+    accordion.querySelector("i").classList.toggle("open");
+  });
+});
+
+// tabs
+
+// const tabsNav = document.querySelectorAll(".tabs__link");
+// const tabsContent = document.querySelectorAll(".tabs__image");
+
+// tabsNav.forEach((tab) => {
+//   tab.addEventListener("click", () => {
+//     removeActiveTab();
+//     tab.classList.add("active");
+//     const activeContent = document.querySelector(`#${tab.id}-img`);
+//     removeActiveContent();
+//     activeContent.classList.add("active");
+//   });
+// });
+
+// function removeActiveTab() {
+//   tabsNav.forEach((tab) => {
+//     tab.classList.remove("active");
+//   });
+// }
+
+// function removeActiveContent() {
+//   tabsContent.forEach((tab) => {
+//     tab.classList.remove("active");
+//   });
+// }
